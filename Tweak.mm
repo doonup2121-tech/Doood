13af4ae1b09fae3a1f90741621e08b62ad405d26
doonup2121-tech <doonup2121@gmail.com>
2026-01-07 06:58:47 +0000

    kkkl

--- Tweak.mm
+++ Tweak.mm
@@ -2,19 +2,42 @@
 #import <objc/runtime.h>
 #import <dlfcn.h>
 
-// دالة التعديل الآمنة
-void applySafeDoonPatch() {
-    // تعديل UILabel فقط (مستحيل يسبب كراش لأنه نظام iOS)
-    Method setText = class_getInstanceMethod([UILabel class], @selector(setText:));
-    if (setText) {
-        IMP originalImp = method_getImplementation(setText);
-        method_setImplementation(setText, imp_implementationWithBlock(^(UILabel *self, NSString *text) {
-            if (text && text.length > 0) {
+// دالة تصنيع ملف تفعيل وهمي (بيانات 2036)
+void createFakeDoonLicense() {
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *docsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    
+    // بنلف على الملفات اللي جوه الفولدر
+    NSArray *contents = [fileManager contentsOfDirectoryAtPath:docsPath error:nil];
+    for (NSString *item in contents) {
+        NSString *fullPath = [docsPath stringByAppendingPathComponent:item];
+        
+        // لو لقينا ملف الكود أو الترخيص
+        if ([item.lowercaseString containsString:@"key"] || [item.lowercaseString containsString:@"lic"]) {
+            NSError *error;
+            NSString *data = [NSString stringWithContentsOfFile:fullPath encoding:NSUTF8StringEncoding error:&error];
+            
+            if (data) {
+                // تعديل التاريخ داخل الملف لـ 2036
+                NSString *newData = [data stringByReplacingOccurrencesOfString:@"2026" withString:@"2036"];
+                [newData writeToFile:fullPath atomically:YES encoding:NSUTF8StringEncoding error:nil];
+            }
+        }
+    }
+}
+
+// دالة تعديل الاسم في القائمة
+void patchDoonBranding() {
+    Method original = class_getInstanceMethod([UILabel class], @selector(setText:));
+    if (original) {
+        IMP originalImp = method_getImplementation(original);
+        method_setImplementation(original, imp_implementationWithBlock(^(UILabel *self, NSString *text) {
+            if (text) {
                 if ([text.lowercaseString containsString:@"pixel"] || [text.lowercaseString containsString:@"raid"]) {
                     text = @"DOON RAID IOS ✅";
                 }
-                if ([text containsString:@"202"] || [text.lowercaseString containsString:@"expire"]) {
-                    text = @"Key expire: 01.01.2036 00:00";
+                if ([text containsString:@"2026"]) {
+                    text = [text stringByReplacingOccurrencesOfString:@"2026" withString:@"2036"];
                 }
             }
             ((void (*)(id, SEL, NSString *))originalImp)(self, @selector(setText:), text);
@@ -22,25 +45,18 @@ void applySafeDoonPatch() {
     }
 }
 
-__attribute__((constructor)) static void doonSafeLoader() {
-    // تحميل المكتبة الأصلية
+__attribute__((constructor)) static void doonUltimateEntry() {
+    // تحميل مكتبة الكراك
     dlopen("@executable_path/wizardcrackv2.dylib", RTLD_NOW);
 
-    // الانتظار وقت طويل (10 ثواني) لضمان انتهاء الشاشة السوداء تماماً
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        applySafeDoonPatch();
-        
-        // محاولة كسر الحماية لكلاس "Wizard" فقط لأنه الأكثر استقراراً
-        Class cls = objc_getClass("Wizard");
-        if (cls) {
-            SEL s1 = @selector(isExpired);
-            if (class_getInstanceMethod(cls, s1)) {
-                class_replaceMethod(cls, s1, imp_implementationWithBlock(^BOOL(id self) { return NO; }), "B@:");
-            }
-            SEL s2 = @selector(checkDevice);
-            if (class_getInstanceMethod(cls, s2)) {
-                class_replaceMethod(cls, s2, imp_implementationWithBlock(^BOOL(id self) { return YES; }), "B@:");
-            }
+    // فحص وتعديل الملفات كل 3 ثواني لضمان كسر الحماية
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        for (int i = 0; i < 20; i++) {
+            [NSThread sleepForTimeInterval:3.0];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                createFakeDoonLicense();
+                patchDoonBranding();
+            });
         }
     });
 }
