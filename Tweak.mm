e0d4592767797b960369bc43c68ccd656f13867e
doonup2121-tech <doonup2121@gmail.com>
2026-01-07 05:59:14 +0000

    hkkjj

--- Tweak.mm
+++ Tweak.mm
@@ -2,49 +2,49 @@
 #import <objc/runtime.h>
 #import <dlfcn.h>
 
-// دالة حديثة لإظهار الرسائل وتعديل الواجهة بدون keyWindow
-void doonPublicPatch(NSString *newDate) {
+// دالة حديثة لإظهار الرسائل وتعديل الواجهة متوافقة مع iOS 18.5
+void applyDoonPatch() {
     dispatch_async(dispatch_get_main_queue(), ^{
-        UIWindow *win = nil;
-        for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
-            if (scene.activationState == UISceneActivationStateForegroundActive) {
-                win = scene.windows.firstObject; break;
-            }
-        }
-        
-        // تعديل أي نص (Label) يحتوي على تاريخ ليصبح 2036
+        // تعديل أي نص يظهر في القائمة (صورة 1131)
         Method original = class_getInstanceMethod([UILabel class], @selector(setText:));
         IMP originalImp = method_getImplementation(original);
+        
         method_setImplementation(original, imp_implementationWithBlock(^(UILabel *self, NSString *text) {
-            if ([text containsString:@"202"]) { // صيد أي تاريخ يبدأ بـ 202
-                text = newDate; 
+            // صيد سطر التاريخ "Key expire" وتغييره لـ 2036
+            if ([text containsString:@"Key expire"] || [text containsString:@"22.01.2026"]) {
+                text = @"Key expire: 01.01.2036 00:00 ✅";
             }
             ((void (*)(id, SEL, NSString *))originalImp)(self, @selector(setText:), text);
         }));
+
+        // إظهار تنبيه النجاح بطريقة متوافقة
+        UIWindowScene *scene = (UIWindowScene *)[[[UIApplication sharedApplication] connectedScenes] anyObject];
+        UIWindow *window = scene.windows.firstObject;
         
-        if (win.rootViewController) {
-            UIAlertController *a = [UIAlertController alertControllerWithTitle:@"DooN Unlocked" message:@"Public Version: Activated Forever! ✅" preferredStyle:1];
-            [a addAction:[UIAlertAction actionWithTitle:@"Enjoy" style:0 handler:nil]];
-            [win.rootViewController presentViewController:a animated:YES completion:nil];
+        if (window.rootViewController) {
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"DooN Edition" 
+                                          message:@"Bypassed & Extended to 2036! 🚀" 
+                                          preferredStyle:UIAlertControllerStyleAlert];
+            [alert addAction:[UIAlertAction actionWithTitle:@"Enjoy" style:UIAlertActionStyleDefault handler:nil]];
+            [window.rootViewController presentViewController:alert animated:YES completion:nil];
         }
     });
 }
 
-__attribute__((constructor)) static void initDoonEngine() {
+__attribute__((constructor)) static void startGlobalCrack() {
+    // فتح المكتبة الأصلية
     dlopen("@executable_path/wizardcrackv2.dylib", RTLD_NOW);
-    
+
+    // تنفيذ التعديلات بعد 4 ثواني لضمان تحميل القائمة
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        doonPublicPatch(@"Key expire: 01.01.2036 13:15");
+        applyDoonPatch();
         
-        Class cls = objc_getClass("Wizard");
+        // كسر حماية الـ VIP وربط الجهاز (HWID)
+        Class cls = objc_getClass("Wizard"); // اسم الكلاس من الصورة
         if (cls) {
-            // كسر الحماية للأبد ولأي جهاز
-            IMP yes = imp_implementationWithBlock(^BOOL(id s){ return YES; });
-            IMP no = imp_implementationWithBlock(^BOOL(id s){ return NO; });
-            
-            class_replaceMethod(cls, @selector(isExpired), no, "B@:");
-            class_replaceMethod(cls, @selector(checkDevice), yes, "B@:");
-            class_replaceMethod(cls, @selector(isVip), yes, "B@:");
+            class_replaceMethod(cls, @selector(isExpired), imp_implementationWithBlock(^BOOL(id self) { return NO; }), "B@:");
+            class_replaceMethod(cls, @selector(checkDevice), imp_implementationWithBlock(^BOOL(id self) { return YES; }), "B@:");
+            class_replaceMethod(cls, @selector(isVip), imp_implementationWithBlock(^BOOL(id self) { return YES; }), "B@:");
         }
     });
 }
