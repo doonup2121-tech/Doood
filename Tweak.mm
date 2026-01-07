e600f71d36b48175a1d3cae131e99ed73296a0aa
doonup2121-tech <doonup2121@gmail.com>
2026-01-07 09:29:17 +0000

    ggg

--- Tweak.mm
+++ Tweak.mm
@@ -1,19 +1,30 @@
 #import <UIKit/UIKit.h>
 #import <dlfcn.h>
+#import <mach/mach.h>
 
-__attribute__((constructor)) static void startDoonFinal() {
-    // تحميل المكتبات بطريقة لا تترك متغيرات غير مستخدمة
-    if (dlopen("@executable_path/Frameworks/Wizard.framework/Wizard", RTLD_NOW)) {
-        // تم التحميل
+// دالة فورية لتعطيل أمر الخروج تماماً
+void freezeExit() {
+    void* exit_ptr = dlsym(RTLD_DEFAULT, "exit");
+    if (exit_ptr) {
+        // تعليمات برمجية تجبر الدالة على العودة فوراً دون تنفيذ الخروج
+        uint32_t ret_inst = 0xD65F03C0; 
+        mach_port_t task = mach_task_self();
+        vm_protect(task, (vm_address_t)exit_ptr, 4, NO, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+        memcpy(exit_ptr, &ret_inst, 4);
+        vm_protect(task, (vm_address_t)exit_ptr, 4, NO, VM_PROT_READ | VM_PROT_EXECUTE);
     }
-    
-    if (dlopen("@executable_path/wizardcrackv2.dylib", RTLD_NOW)) {
-        // تم التحميل
-    }
+}
 
-    // الانتظار لإظهار الرسالة
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(15.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        
+__attribute__((constructor)) static void doonStrike() {
+    // 1. تجميد دالة الخروج أولاً وقبل أي شيء
+    freezeExit();
+
+    // 2. تحميل المكتبات
+    dlopen("@executable_path/Frameworks/Wizard.framework/Wizard", RTLD_NOW);
+    dlopen("@executable_path/wizardcrackv2.dylib", RTLD_NOW);
+
+    // 3. إظهار رسالة النجاح بعد 5 ثوانٍ فقط
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIWindow *window = nil;
         if (@available(iOS 13.0, *)) {
             for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
@@ -23,14 +34,12 @@ __attribute__((constructor)) static void startDoonFinal() {
                 }
             }
         }
-        
         if (!window) window = [UIApplication sharedApplication].windows.firstObject;
 
         if (window && window.rootViewController) {
             UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"DooN Raid" 
-                                          message:@"System Ready ✅\nWait for menu..." 
+                                          message:@"Anti-Exit: ACTIVE ✅\nTry any code now." 
                                           preferredStyle:UIAlertControllerStyleAlert];
-            
             [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
             [window.rootViewController presentViewController:alert animated:YES completion:nil];
         }
