3999a623d748d3096b514737b4797404f509a5b5
doonup2121-tech <doonup2121@gmail.com>
2026-01-07 06:37:27 +0000

    jdjdfk

--- Tweak.mm
+++ Tweak.mm
@@ -2,19 +2,17 @@
 #import <objc/runtime.h>
 #import <dlfcn.h>
 
-// دالة التبديل الإجباري - لا تعتمد على أسماء الكلاسات
-void forceDoonPatch() {
-    // 1. سيطرة كاملة على نصوص الواجهة (الاسم والتاريخ)
+// دالة التعديل الآمنة
+void applySafeDoonPatch() {
+    // تعديل UILabel فقط (مستحيل يسبب كراش لأنه نظام iOS)
     Method setText = class_getInstanceMethod([UILabel class], @selector(setText:));
     if (setText) {
         IMP originalImp = method_getImplementation(setText);
         method_setImplementation(setText, imp_implementationWithBlock(^(UILabel *self, NSString *text) {
             if (text && text.length > 0) {
-                // تبديل اسم الهاك فوراً
                 if ([text.lowercaseString containsString:@"pixel"] || [text.lowercaseString containsString:@"raid"]) {
                     text = @"DOON RAID IOS ✅";
                 }
-                // تبديل التاريخ فوراً لـ 2036
                 if ([text containsString:@"202"] || [text.lowercaseString containsString:@"expire"]) {
                     text = @"Key expire: 01.01.2036 00:00";
                 }
@@ -22,39 +20,27 @@ void forceDoonPatch() {
             ((void (*)(id, SEL, NSString *))originalImp)(self, @selector(setText:), text);
         }));
     }
-
-    // 2. كسر حماية الـ VIP والجهاز (البحث عن الوظائف بالمعنى وليس بالاسم)
-    int numClasses = objc_getClassList(NULL, 0);
-    Class *classes = (Class *)malloc(sizeof(Class) * numClasses);
-    numClasses = objc_getClassList(classes, numClasses);
-
-    for (int i = 0; i < numClasses; i++) {
-        unsigned int mCount;
-        Method *methods = class_copyMethodList(classes[i], &mCount);
-        for (unsigned int j = 0; j < mCount; j++) {
-            NSString *selName = NSStringFromSelector(method_getName(methods[j])).lowercaseString;
-            
-            // أي دالة بترجع Bool وليها علاقة بالأمان هيتم تعديلها
-            if ([selName containsString:@"expired"] || [selName containsString:@"device"] || [selName containsString:@"vip"]) {
-                class_replaceMethod(classes[i], method_getName(methods[j]), imp_implementationWithBlock(^BOOL(id self) {
-                    return [selName containsString:@"expired"] ? NO : YES;
-                }), "B@:");
-            }
-        }
-        free(methods);
-    }
-    free(classes);
 }
 
-// محرك التشغيل الأساسي
-__attribute__((constructor)) static void loadDoon() {
-    // تحميل مكتبة الهاك الأصلية أولاً
+__attribute__((constructor)) static void doonSafeLoader() {
+    // تحميل المكتبة الأصلية
     dlopen("@executable_path/wizardcrackv2.dylib", RTLD_NOW);
 
-    // تكرار محاولة الحقن كل ثانية لضمان النجاح بعد تحميل القائمة
-    for (int t = 1; t <= 5; t++) {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(t * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            forceDoonPatch();
-        });
-    }
+    // الانتظار وقت طويل (10 ثواني) لضمان انتهاء الشاشة السوداء تماماً
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        applySafeDoonPatch();
+        
+        // محاولة كسر الحماية لكلاس "Wizard" فقط لأنه الأكثر استقراراً
+        Class cls = objc_getClass("Wizard");
+        if (cls) {
+            SEL s1 = @selector(isExpired);
+            if (class_getInstanceMethod(cls, s1)) {
+                class_replaceMethod(cls, s1, imp_implementationWithBlock(^BOOL(id self) { return NO; }), "B@:");
+            }
+            SEL s2 = @selector(checkDevice);
+            if (class_getInstanceMethod(cls, s2)) {
+                class_replaceMethod(cls, s2, imp_implementationWithBlock(^BOOL(id self) { return YES; }), "B@:");
+            }
+        }
+    });
 }
